410d0ddc1f1c692224f3a353c80bd6707413f253
Douglas Bolden <douglas.bolden@ymail.com>
2025-03-19 01:55:32 -0400

    Created Todo files to create a todo list - Added Todo link in Nav Menu

--- obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net9.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e55eeb9606f43fd112ea754680840658babae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
